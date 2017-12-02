6e83c37fab10f451e311c859434e271b7a423461
Sherly Sun <cynosure@ucla.edu>
2017-12-02 10:21:05 -0800

    Add details

--- similarity-based/prediciton-model.py
+++ similarity-based/prediciton-model.py
@@ -18,10 +18,14 @@ def predict_single_case(line,train_file):
 	trip_id,start_time,day_type,nearest_stop = element_list[0],int(element_list[1]),element_list[2],int(element_list[3])
 	coor_list = np.array(ast.literal_eval(element_list[4]))
 
+
+
 	train_content = open(train_file,'r')
 	train_content.readline()
 	for train_line in train_content.readlines():
 		train_element_list = line.split('"')[1:2]
+	return
+
 		
 
 
@@ -34,6 +38,7 @@ def predict_test(test_file,train_file):
 	test_content.readline()
 	for line in test_content.readlines():
 		result = predict_single_case(line,train_file)
+	return
 
 
 		
